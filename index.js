ec6de5fd4ffd08f292e4bcc57a70e4807e4d4324
StephanieMfon <108840244+StephanieMfon@users.noreply.github.com>
2023-02-13 01:54:30 +0530

    Added Markup and Script files and updated entire Repository contents with neccesary details.

---
+++ index.js
@@ -0,0 +1,21 @@
+//  MULTIPLY TWO NUMBERS 
+
+const multiplyTwoNumbers = (a,b)=>{return `Result:${a * b}`}
+console.log(multiplyTwoNumbers(56,3))
+
+
+// PERSONAL DETAILS
+
+const firstName = "Stephanie"
+const lastName = "Okpo-Mfon"
+const stuternTrack = "Backend Development"
+const technicalSkills  = "HTML, CSS, JavaScript, GIT, UI Design, Jquery"
+const softSkills = "Emotional Intelligence, Time Management, Critical Thinking, Public Relations"
+const favouriteColor = "dark blue"
+const independenceDate = 1960
+
+const summary = `My first name is ${firstName}👸🏿 and my last name is ${lastName}.
+My 💻Stutern track is ${stuternTrack}. My soft skills are ${softSkills} in addition to ${technicalSkills} as my technical skills,
+My favourite color is 🟦${favouriteColor}. I'm Nigerian, and my country's independence date is ${independenceDate}.`
+
+console.log(summary)
